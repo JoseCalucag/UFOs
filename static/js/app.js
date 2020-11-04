0c40db417161a5d3989b2939bb8da114f65f8706
Jose Calucag <josecalucag@Joses-MacBook-Pro.local>
2020-11-04 04:19:04 -0500

    updating project

--- static/js/app.js
+++ static/js/app.js
@@ -5,23 +5,24 @@ const tableData = data;
 var tbody = d3.select("tbody"); 
 
 function buildTable(data) {
-// Clear out any existing daya
+    // First, clear out any existing data
     tbody.html("");
-
-// Loop through each object in data
-// and append a row and cells for each value in row
-data.forEach((dataRow) => {
-    // Append a row to the table body
-    let row = tbody.append("tr");
-        
-    // Loop through each field in the dataRow and add
-    // each val as a table cell ("td")        
-    Object.values(dataRow).forEach((val) => {
+  
+    // Next, loop through each object in the data
+    // and append a row and cells for each value in the row
+    data.forEach((dataRow) => {
+      // Append a row to the table body
+      let row = tbody.append("tr");
+  
+      // Loop through each field in the dataRow and add
+      // each value as a table cell (td)
+      Object.values(dataRow).forEach((val) => {
         let cell = row.append("td");
         cell.text(val);
         }
-    );
-});
+      );
+    });
+  }
 
 function handleClick() {
     // Grab the datetime value from the filter
@@ -46,4 +47,4 @@ function handleClick() {
 d3.selectAll("#filter-btn").on("click", handleClick);
 
 // Build the table when the page loads
-//buildTable(tableData);
+buildTable(tableData);
